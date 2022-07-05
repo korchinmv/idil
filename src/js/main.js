84bc3093b08f80add453a7299e89f3ca99315645
korchinmv <korchinmv@yandex.ru>
2022-07-05 12:28:13 +0300

    added about-block + adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import './_functions';
 import './functions/burger';
 import './functions/dark-theme';
 import './functions/sliders';
+import './functions/accordion';
+import './functions/tabs';
 import './functions/func';
 
 
